78b01c7029b8547372ece0daa300f66711812884
seongukHeo <heoseonguk3@gmail.com>
2024-05-06 11:01:01 +0900

    feat : MediaPlayer를 활용하여 기능 구현

--- app/src/main/java/kr/co/seonguk/application/fastmusicplayer/MainActivity.kt
+++ app/src/main/java/kr/co/seonguk/application/fastmusicplayer/MainActivity.kt
@@ -1,11 +1,67 @@
 package kr.co.seonguk.application.fastmusicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.seonguk.application.fastmusicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    private var mediaPlayer : MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        settingEvent()
+    }
+
+
+    private fun settingEvent(){
+        binding.apply {
+            buttonPlay.setOnClickListener {
+                mediaPlayerPlay()
+            }
+
+            buttonStop.setOnClickListener {
+                mediaPlayerStop()
+            }
+
+            buttonPause.setOnClickListener {
+                mediaPlayerPause()
+            }
+        }
+    }
+
+    private fun mediaPlayerPlay(){
+        if (mediaPlayer == null){
+            mediaPlayer = MediaPlayer.create(this, R.raw.music_my).apply {
+                isLooping = true
+            }
+            mediaPlayer?.start()
+        }
+    }
+
+    private fun mediaPlayerPause(){
+        mediaPlayer?.pause()
+    }
+
+    private fun mediaPlayerStop(){
+        mediaPlayer?.stop()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
